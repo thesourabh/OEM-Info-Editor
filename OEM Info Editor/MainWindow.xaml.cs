1c2338b474342eed709a02b1803e7b35ca7c15be
Sourabh Shetty <sourabhshetty@gmail.com>
2015-08-07 02:38:41 +0400

    first build

---
+++ OEM Info Editor/MainWindow.xaml.cs
@@ -0,0 +1,164 @@
+﻿
+using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace OEM_Info_Editor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        /// <summary>
+        /// Singleton class to control access to single registry key
+        /// </summary>
+        private class OEMKey
+        {
+            
+            private static RegistryKey key;
+            private OEMKey() { }
+            public static RegistryKey GetKey()
+            {
+                if (key == null)
+                {
+                    string keyPath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation";
+                    RegistryKey baseKey = Registry.LocalMachine;
+                    if (Environment.Is64BitOperatingSystem)
+                    {
+                        // Workaround for 64 bit systems since a 32 bit process will access the 32 bit registry by default
+                        baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+                    }
+                    RegistryKey key = baseKey.CreateSubKey(keyPath);
+                    return key;
+                }
+                else
+                {
+                    return key;
+                }
+            }
+            public static string GetStringFromKey(string name)
+            {
+                if (key == null)
+                    key = GetKey();
+                object val = key.GetValue(name);
+                if (val == null)
+                    return "";
+                try
+                {
+                    string valString = (string)val;
+                    return valString;
+                }
+                catch
+                {
+                    return "";
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetCurrentInformation();
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select BMP file";
+            ofd.Filter = "BMP|*.bmp";
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            if (ofd.ShowDialog() == true)
+            {
+                string imageUrl = ofd.FileName;
+                SetLogoValues(imageUrl);
+            }
+        }
+
+        private void btnClearLogo_Click(object sender, RoutedEventArgs e)
+        {
+            ClearLogo();
+        }
+
+        private void btnClearAll_Click(object sender, RoutedEventArgs e)
+        {
+            ClearAll();
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            GetCurrentInformation();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            SetUpdatedInformation();
+        }
+
+        private void ClearLogo()
+        {
+            SetLogoValues("");
+        }
+
+        private void ClearAll()
+        {
+            ClearLogo();
+            txtManufacturer.Text = "";
+            txtModel.Text = "";
+            txtSupportHours.Text = "";
+            txtSupportPhone.Text = "";
+            txtSupportUrl.Text = "";
+        }
+
+        private void SetLogoValues(string imageUrl)
+        {
+            try {
+                imgLogo.Source = new BitmapImage(new Uri(imageUrl));
+                lblLogoName.Content = imageUrl.Substring(imageUrl.LastIndexOf('\\') + 1);
+                imgLogo.Tag = imageUrl;
+            } catch (Exception) {
+                imageUrl = "";
+                lblLogoName.Content = "";
+                imgLogo.Tag = "";
+                imgLogo.Source = null;
+            }
+
+        }
+
+        private void GetCurrentInformation()
+        {
+            RegistryKey key = OEMKey.GetKey();
+            txtManufacturer.Text = OEMKey.GetStringFromKey("Manufacturer");
+            txtModel.Text = OEMKey.GetStringFromKey("Model");
+            txtSupportHours.Text = OEMKey.GetStringFromKey("SupportHours");
+            txtSupportPhone.Text = OEMKey.GetStringFromKey("SupportPhone");
+            txtSupportUrl.Text = OEMKey.GetStringFromKey("SupportURL");
+            SetLogoValues(OEMKey.GetStringFromKey("Logo"));
+        }
+
+        private void SetUpdatedInformation()
+        {
+            string manufacturer, model, supportHours, supportPhone, supportUrl, logo;
+            manufacturer = txtManufacturer.Text;
+            model = txtModel.Text;
+            supportHours = txtSupportHours.Text;
+            supportPhone = txtSupportPhone.Text;
+            supportUrl = txtSupportUrl.Text;
+            logo = (string)imgLogo.Tag;
+            RegistryKey key = OEMKey.GetKey();
+            key.SetValue("Manufacturer", manufacturer);
+            key.SetValue("Model", model);
+            key.SetValue("SupportHours", supportHours);
+            key.SetValue("SupportPhone", supportPhone);
+            key.SetValue("SupportURL", supportUrl);
+            if (logo == null)
+                key.SetValue("Logo", "");
+            else
+                key.SetValue("Logo", logo);
+            string cplPath = System.IO.Path.Combine(Environment.SystemDirectory, "control.exe");
+            System.Diagnostics.Process.Start(cplPath, "/name Microsoft.System");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# OEM Info Editor
+OEM Info Editor is a portable tool that allows you to edit/add OEM fields to Windows System properties.
+
+---
+## Usage:
+* In case you've done a fresh install, and would like to store your OEM information in the System Properties.
+* In case you would like store the OEM's support contact information in a convenient location.
+* In case you want the System window to say that you yourself are the manufacturer, or would like to add custom information.
+* In case you'd like to change these things, but don't want to deal with the Registry yourself.
+* In case you are an OEM, and yet inexplicably don't know how to work the Windows Registry.
+---
+![something cool](http://i.imgur.com/v4ss3E1.png)
+
+---
+* Tested on Windows 10 64 bit system, more on the way.
+* .NET Framework 4.5 required.
