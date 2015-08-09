3ae595086aef791c907dcbbcd2c1845d2ea251c6
Sourabh Shetty <sourabhshetty@gmail.com>
2015-08-09 19:22:43 +0400

    fix for system32/sysnative on 64-bit machines, and for HelpCustomized DWORD blocking display

--- OEM Info Editor/MainWindow.xaml.cs
+++ OEM Info Editor/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿
 using Microsoft.Win32;
 using System;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Media.Imaging;
 
@@ -63,7 +65,6 @@ namespace OEM_Info_Editor
             InitializeComponent();
             GetCurrentInformation();
         }
-
         private void btnBrowse_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
@@ -135,7 +136,19 @@ namespace OEM_Info_Editor
             txtSupportHours.Text = OEMKey.GetStringFromKey("SupportHours");
             txtSupportPhone.Text = OEMKey.GetStringFromKey("SupportPhone");
             txtSupportUrl.Text = OEMKey.GetStringFromKey("SupportURL");
-            SetLogoValues(OEMKey.GetStringFromKey("Logo"));
+            string system32Directory = Path.Combine(Environment.ExpandEnvironmentVariables("%windir%"), "system32");
+            string logo = OEMKey.GetStringFromKey("Logo");
+            if (logo.IndexOf(system32Directory, StringComparison.OrdinalIgnoreCase) >= 0)
+            {
+                if (Environment.Is64BitOperatingSystem)
+                {
+                    // For 32-bit processes on 64-bit systems, %windir%\system32 folder
+                    // can only be accessed by specifying %windir%\sysnative folder.
+                    string sysnative = Path.Combine(Environment.ExpandEnvironmentVariables("%windir%"), "sysnative");
+                    logo = Regex.Replace(logo, "system32", "sysnative", RegexOptions.IgnoreCase);
+                }
+            }
+            SetLogoValues(logo);
         }
 
         private void SetUpdatedInformation()
@@ -148,6 +161,12 @@ namespace OEM_Info_Editor
             supportUrl = txtSupportUrl.Text;
             logo = (string)imgLogo.Tag;
             RegistryKey key = OEMKey.GetKey();
+
+            // Set value of HelpCustomized to false (0) ONLY if it exists and is true (1).
+            // Default is false.
+            if ((int)key.GetValue("HelpCustomized", 0) == 1)
+                key.SetValue("HelpCustomized", 0, RegistryValueKind.DWord);
+
             key.SetValue("Manufacturer", manufacturer);
             key.SetValue("Model", model);
             key.SetValue("SupportHours", supportHours);
@@ -156,7 +175,19 @@ namespace OEM_Info_Editor
             if (logo == null)
                 key.SetValue("Logo", "");
             else
+            {
+                string system32Directory = Path.Combine(Environment.ExpandEnvironmentVariables("%windir%"), "sysnative");
+                if (logo.IndexOf(system32Directory, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    if (Environment.Is64BitOperatingSystem)
+                    {
+                        // For 32-bit processes on 64-bit systems, %windir%\system32 folder
+                        // can only be accessed by specifying %windir%\sysnative folder.
+                        logo = Regex.Replace(logo, "sysnative", "system32", RegexOptions.IgnoreCase);
+                    }
+                }
                 key.SetValue("Logo", logo);
+            }
             string cplPath = System.IO.Path.Combine(Environment.SystemDirectory, "control.exe");
             System.Diagnostics.Process.Start(cplPath, "/name Microsoft.System");
         }
